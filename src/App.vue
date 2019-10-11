9c264d391c07b8edb5bc8dcbda47b88ea6689363
gerald <geraldine.antonelli@gmail.com>
2019-10-11 17:17:06 +0100

    fecth is not working

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+  <div class="">
+    <h2>Air Quality App</h2>
+    <div class="wrapper">
+      <air-countries :countries='countries'></air-countries>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import {eventBus} from './main.js';
+import AirCountries from './components/AirCountries.vue';
+import AirCountriesName from './components/AirCountriesName.vue';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      countries: [],
+      cities: [],
+      pollution: null
+
+    };
+  },
+  mounted(){
+    fetch('http://api.airvisual.com/v2/countries?key=c3dd44df-eff9-44b8-9f22-eb5fee8a4789')
+    .then(response => response.json())
+    .then(countries => this.countries = countries)
+
+    // eventBus.$on('country-selected', (country) =>{
+    //   this.seelectedCountry = country;
+    // })
+
+
+  },
+
+  components: {
+    "air-countries": AirCountries,
+    "air-countries-name": AirCountriesName
+
+  }
+
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/AirCountries.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="">
+    <h3></h3>
+    <span>
+      <air-countries-name v-for="(country, index) in countries" :country="country" :key="index" ></air-countries-name>
+    </span>
+  </div>
+</template>
+
+<script>
+import AirCountriesName from './AirCountriesName.vue'
+
+export default {
+  name:'air-countries',
+  props: ['countries'],
+  components:{
+    "air-countries-name": AirCountriesName
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
